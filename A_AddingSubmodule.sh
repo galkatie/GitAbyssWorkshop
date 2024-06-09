9db3fa0522ace0f427201d95e3c3bed78f1b76d3
Rafal Karwat <rafal.karwat@stonex.com>
2024-06-09 21:56:03 +0200

    Uncomment commented AddingSubmoduleScript

--- A_AddingSubmodule.sh
+++ A_AddingSubmodule.sh
@@ -4,23 +4,23 @@ if [ "$(basename $(pwd))" != "me" ]; then
     exit 1
 fi
 
-# git submodule add ../AbyssWorkshopOrigins/PartnerRepo plugins/partner
-# echo "Check the state of ./plugins/partner/. Does it contain partner files?"
+git submodule add ../AbyssWorkshopOrigins/PartnerRepo plugins/partner
+echo "Check the state of ./plugins/partner/. Does it contain partner files?"
 
-# read -n 1 -s
+read -n 1 -s
 
 
-# git status
-# echo "What has changed? What is the status of the submodule?"
+git status
+echo "What has changed? What is the status of the submodule?"
 
-# read -n 1 -s
+read -n 1 -s
 
-# git status --ignore-submodules=none
-# echo "What is the status of the submodule now?"
+git status --ignore-submodules=none
+echo "What is the status of the submodule now?"
 
-# read -n 1 -s
+read -n 1 -s
 
-# git add -A
-# git commit -m "Add submodule"
-# git push
-# echo "Publishing newly added submodule is very intuitive."
+git add -A
+git commit -m "Add submodule"
+git push
+echo "Publishing newly added submodule is very intuitive."
