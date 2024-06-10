2802891d5cb84791cc9d7baebbcc2c8acd5f6ffc
Katarzyna Galwa <galwa.katarzyna@gmail.com>
2024-06-10 20:31:26 +0200

    Rename Exercise 4

--- Dont look up/Exercise 6 - Bisecting bugs/D - Solution/bisect_typo.sh
+++ Dont look up/Exercise 6 - Bisecting bugs/D - Solution/bisect_typo.sh
@@ -4,10 +4,13 @@ file_path="./GitBisect/Page.html"
 search_value="Secction"
 
 # Use grep to search for the value in the file
-if grep -q "$search_value" "$file_path"; then
+if grep -qi "$search_value" "$file_path"; then 
     # echo "Value found"
     exit 1
 else
     # echo "Value not found"
     exit 0
 fi
+
+
+
